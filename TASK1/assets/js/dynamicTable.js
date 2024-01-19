22f0933c8b435f3e0436f226f49e799b9902a40e
Deepakkumar8130 <dk0802270@gmail.com>
2024-01-19 19:30:54 +0530

    Dynamic table Generator

--- TASK1/assets/js/dynamicTable.js
+++ TASK1/assets/js/dynamicTable.js
@@ -1,2 +1,35 @@
-console.log("hello");
-    alert("check");
+
+let rows = parseInt(prompt("Enter no. of rows : ","ENTER ROWS"));
+let cols = parseInt(prompt("Enter no. of columns : ","ENTER COLS"));
+
+// console.log(rows);
+// console.log(cols);
+
+
+function generateTable(){
+    let table = "";
+    for(let i=1; i<=rows; i++){
+        table += "<tr>";
+        for(let j=1; j<=cols; j++){
+            table +=`<td>${j}</td>`;
+        }
+        table +="</tr>";
+    }
+    return table;
+}
+
+document.write(`
+
+    <div class='container-fluid m-5'>
+        <div class='card'>
+            <div class='card-body'>
+                <table class='table table-bordered'>
+                    <tbody>
+                        ${generateTable()}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+`);
